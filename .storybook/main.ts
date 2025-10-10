4b4a0163cac2504b6fd3d8ea029f4cb93fceb10c
Diego Villanueva <diego.villanueva@pss-ti.com>
2025-10-09 23:52:18 -0500

    feat: Install storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  core: {
+    builder: "@storybook/builder-vite"
+  },
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  },
+  async viteFinal(config) {
+    return config;
+  }
+};
+export default config;
