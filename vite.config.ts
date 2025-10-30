26cbe10aeb53e9057fed6f5eba5edb2ce266100b
Diego Villanueva <diego.villanueva@pss-ti.com>
2025-10-30 01:02:46 -0500

    feat: Load css in bundler

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({
       tsconfigPath: "./tsconfig.app.json",
       // entryRoot: "src",
